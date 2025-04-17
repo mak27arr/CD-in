9b3ea8d0f99702360ba440a4c61db1cabc19aa7b
andrii makar <mak27arr@gmail.com>
2025-04-17 23:48:40 +0300

    refactoring

---
+++ CD-in/ViewModel/MainViewModel.Properties.cs
@@ -0,0 +1,219 @@
+﻿using System.IO;
+
+namespace CD_in
+{
+    public partial class MainViewModel
+    {
+        #region variables
+
+        private bool _isExecuting;
+        private double _progress;
+        private int _blockSize = 100000;
+        private bool _isStatisticsChecked;
+        private bool _isWithoutCdOutChecked;
+        private string? _inFolderPath;
+        private bool _isMergeChecked;
+        private bool _isMergeSecondChecked;
+        private bool _isReplaceChecked;
+        private bool _isLargerChecked;
+        private int _largerNumberValue = 11;
+        private int _mergeOrderLength = 11;
+        private int _mergeSecondOrderLength = 6;
+        private int _mergeOrderExecution = 1;
+        private int _mergeSecondOrderExecution = 2;
+        private int _replaceOrderExecution = 3;
+        private int _largerOrderExecution = 4;
+        private TimeSpan _elapsedTime;
+
+        #endregion
+
+        private bool CanProcessFolder => !IsExecuting && !string.IsNullOrWhiteSpace(CDInFolderPath) && Directory.Exists(CDInFolderPath) && BlockSize > 0;
+
+        public int BlockSize
+        {
+            get => _blockSize;
+            set
+            {
+                _blockSize = value;
+                OnPropertyChanged();
+                ProcessFolderCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        public bool IsStatisticsChecked
+        {
+            get => _isStatisticsChecked;
+            set
+            {
+                _isStatisticsChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsWithoutCdOutChecked
+        {
+            get => _isWithoutCdOutChecked;
+            set
+            {
+                _isWithoutCdOutChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string? CDInFolderPath
+        {
+            get => _inFolderPath;
+            set
+            {
+                _inFolderPath = value;
+                OnPropertyChanged();
+                ProcessFolderCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        public bool IsExecuting
+        {
+            get => _isExecuting;
+            set
+            {
+                _isExecuting = value;
+                OnPropertyChanged(nameof(IsExecuting));
+                ProcessFolderCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        public double Progress
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                OnPropertyChanged(nameof(Progress));
+            }
+        }
+
+        public TimeSpan ElapsedTime
+        {
+            get => _elapsedTime;
+            set
+            {
+                _elapsedTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Option
+
+        public bool IsMergeChecked
+        {
+            get => _isMergeChecked;
+            set
+            {
+                _isMergeChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsMergeSecondChecked
+        {
+            get => _isMergeSecondChecked;
+            set
+            {
+                _isMergeSecondChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsReplaceChecked
+        {
+            get => _isReplaceChecked;
+            set
+            {
+                _isReplaceChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsLargerChecked
+        {
+            get => _isLargerChecked;
+            set
+            {
+                _isLargerChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MergeOrderExecution
+        {
+            get => _mergeOrderExecution;
+            set
+            {
+                _mergeOrderExecution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MergeSecondOrderExecution
+        {
+            get => _mergeSecondOrderExecution;
+            set
+            {
+                _mergeSecondOrderExecution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ReplaceOrderExecution
+        {
+            get => _replaceOrderExecution;
+            set
+            {
+                _replaceOrderExecution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int LargerNumberValue
+        {
+            get => _largerNumberValue;
+            set
+            {
+                _largerNumberValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int LargerOrderExecution
+        {
+            get => _largerOrderExecution;
+            set
+            {
+                _largerOrderExecution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MergeOrderLength
+        {
+            get => _mergeOrderLength;
+            set
+            {
+                _mergeOrderLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MergeSecondOrderLength
+        {
+            get => _mergeSecondOrderLength;
+            set
+            {
+                _mergeSecondOrderLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+    }
+}
--- CD-in/ViewModel/MainViewModel.cs
+++ CD-in/ViewModel/MainViewModel.cs
@@ -5,230 +5,19 @@ using CD_in_Core.Domain.Models.Sequences;
 using CD_in_Core.Domain.Models.Spec;
 using CD_in_Core.Domain.Models;
 using CommunityToolkit.Mvvm.Input;
-using System.ComponentModel;
 using System.IO;
 using System.Windows.Threading;
 using System.Diagnostics;
+using CD_in.ViewModel;
 
 namespace CD_in
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public partial class MainViewModel : BaseViewModel
     {
-        #region variables
-
         private DispatcherTimer _timer;
         private Stopwatch _stopwatch;
         private IFolderProcessingService _folderProcessingService;
         private CancellationTokenSource? tokenSource;
-        private bool _isExecuting;
-        private double _progress;
-        private int _blockSize = 100000;
-        private bool _isStatisticsChecked;
-        private bool _isWithoutCdOutChecked;
-        private string? _inFolderPath;
-        private bool _isMergeChecked;
-        private bool _isMergeSecondChecked;
-        private bool _isReplaceChecked;
-        private bool _isLargerChecked;
-        private int _largerNumberValue = 11;
-        private int _mergeOrderLength = 11;
-        private int _mergeSecondOrderLength = 6;
-        private int _mergeOrderExecution = 1;
-        private int _mergeSecondOrderExecution = 2;
-        private int _replaceOrderExecution = 3;
-        private int _largerOrderExecution = 4;
-        private TimeSpan _elapsedTime;
-
-        #endregion
-
-        public int BlockSize
-        {
-            get => _blockSize;
-            set
-            {
-                _blockSize = value;
-                OnPropertyChanged();
-                ProcessFolderCommand.NotifyCanExecuteChanged();
-            }
-        }
-
-        public bool IsStatisticsChecked
-        {
-            get => _isStatisticsChecked;
-            set
-            {
-                _isStatisticsChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IsWithoutCdOutChecked
-        {
-            get => _isWithoutCdOutChecked;
-            set
-            {
-                _isWithoutCdOutChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string? CDInFolderPath
-        {
-            get => _inFolderPath;
-            set
-            {
-                _inFolderPath = value;
-                OnPropertyChanged();
-                ProcessFolderCommand.NotifyCanExecuteChanged();
-            }
-        }
-
-        public bool IsExecuting
-        {
-            get => _isExecuting;
-            set
-            {
-                _isExecuting = value;
-                OnPropertyChanged(nameof(IsExecuting));
-                ProcessFolderCommand.NotifyCanExecuteChanged();
-            }
-        }
-
-        public double Progress
-        {
-            get => _progress;
-            set
-            {
-                _progress = value;
-                OnPropertyChanged(nameof(Progress));
-            }
-        }
-
-        public TimeSpan ElapsedTime
-        {
-            get => _elapsedTime;
-            set
-            {
-                _elapsedTime = value;
-                OnPropertyChanged();
-            }
-        }
-
-        #region Option
-
-        public bool IsMergeChecked
-        {
-            get => _isMergeChecked;
-            set
-            {
-                _isMergeChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IsMergeSecondChecked
-        {
-            get => _isMergeSecondChecked;
-            set
-            {
-                _isMergeSecondChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IsReplaceChecked
-        {
-            get => _isReplaceChecked;
-            set
-            {
-                _isReplaceChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool IsLargerChecked
-        {
-            get => _isLargerChecked;
-            set
-            {
-                _isLargerChecked = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int MergeOrderExecution
-        {
-            get => _mergeOrderExecution;
-            set
-            {
-                _mergeOrderExecution = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int MergeSecondOrderExecution
-        {
-            get => _mergeSecondOrderExecution;
-            set
-            {
-                _mergeSecondOrderExecution = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int ReplaceOrderExecution
-        {
-            get => _replaceOrderExecution;
-            set
-            {
-                _replaceOrderExecution = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int LargerNumberValue
-        {
-            get => _largerNumberValue;
-            set
-            {
-                _largerNumberValue = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int LargerOrderExecution
-        {
-            get => _largerOrderExecution;
-            set
-            {
-                _largerOrderExecution = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int MergeOrderLength
-        {
-            get => _mergeOrderLength;
-            set
-            {
-                _mergeOrderLength = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int MergeSecondOrderLength
-        {
-            get => _mergeSecondOrderLength;
-            set
-            {
-                _mergeSecondOrderLength = value;
-                OnPropertyChanged();
-            }
-        }
-
-        #endregion
-
-        private bool CanProcessFolder => !IsExecuting && !string.IsNullOrWhiteSpace(CDInFolderPath) && Directory.Exists(CDInFolderPath) && BlockSize > 0;
 
         #region ICommand
 
@@ -266,20 +55,6 @@ namespace CD_in
             IsExecuting = false;
         }
 
-        private void StopMeasurement()
-        {
-            _stopwatch.Stop();
-            _timer.Stop();
-        }
-
-        private void ResetAndStartMeasurement()
-        {
-            UpdateProgress(0);
-            _timer.Start();
-            _stopwatch.Reset();
-            _stopwatch.Start();
-        }
-
         private void Cancel()
         {
             tokenSource?.Cancel();
@@ -299,9 +74,25 @@ namespace CD_in
 
         #endregion
 
+        #region Helper
+
         private void UpdateProgress(double progress)
         {
             Progress = progress;
+        }
+
+        private void ResetAndStartMeasurement()
+        {
+            UpdateProgress(0);
+            _timer.Start();
+            _stopwatch.Reset();
+            _stopwatch.Start();
+        }
+
+        private void StopMeasurement()
+        {
+            _stopwatch.Stop();
+            _timer.Stop();
         }
 
         private ProcessingOption BuildProcessingOption()
@@ -400,11 +191,6 @@ namespace CD_in
             return parentFolder != null ? Path.Combine(parentFolder, "CD-out") : string.Empty;
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        #endregion
     }
 }
